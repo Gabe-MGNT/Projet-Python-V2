fb2218a36183199fc4049c1435b42730a2a60fe8
Gabe-MGNT <107551867+Gabe-MGNT@users.noreply.github.com>
2022-10-19 15:08:48 +0200

    Last Vers

--- Debut_de_projet.py
+++ Debut_de_projet.py
@@ -17,6 +17,7 @@ fig2=px.histogram(
     x=["LIBREG"]
 )
 df["ZE2020"]=df["ZE2020"].apply(lambda x: str(x).rjust(4,'0') if len(str(x))<4 else x)
+
 da = df.iloc[:,3:].groupby("LIBREG").mean()
 da = da.dropna(how = 'any')
 dat= da.T
@@ -24,7 +25,16 @@ fig20=px.histogram(
     dat,
     x=dat.index,
     y=dat.columns,
-    #barmode="overlay"
+    labels={
+        "LIBREG":"Région",
+        "index":"Date",
+        "sum of value":"moyenne"
+    },
+
+).update_yaxes(
+    title_text="Somme des moyennes"
+).update_xaxes(
+    title_text="Dates de relevés"
 )
 
 db = df.iloc[:,3:]
@@ -86,9 +96,33 @@ dcc.Graph(
         id="graph20",
         figure=fig20
     ),
+
+    dcc.Dropdown(
+
+        id="dropdown_histo",
+        options=[
+            {"value": i, "label": i} for i in df["LIBREG"].dropna().unique()
+        ],
+        multi=True,
+        value=["ILE DE FRANCE", "OCCITANIE"]
+
+    ),
 dcc.Graph(
         id="graph21",
         figure=fig21
+    ),
+
+    dcc.Slider(
+
+        1, len(df.columns[5:]),
+        step=1,
+        id="slider_histo",
+        value=1,
+        marks={
+            1: "2003",
+            len(df.columns[5:]): "2022"
+        },
+        updatemode="drag",
     ),
 dcc.Graph(
         id="graph3",
@@ -134,6 +168,45 @@ dcc.Graph(
     ),
     ]
 )
+
+@app.callback(
+    Output(component_id="graph21",component_property="figure"),
+    [Input(component_id="dropdown_histo",component_property="value"),
+     Input(component_id="slider_histo",component_property="value"),]
+)
+def update_histo(dropdown_value,slider_value):
+    list_reg=[]
+    for region in dropdown_value:
+        list_reg.append(region)
+
+    annee=db[db["LIBREG"].isin(list_reg)].columns[slider_value]
+    if list_reg==[] or db[db["LIBREG"].isin(list_reg)]["2003-T1"].empty:
+         return px.histogram(
+             db[db["LIBREG"].isin(list_reg)],
+             x=annee,
+             barmode="relative",
+             color="LIBREG"
+         ).add_annotation(
+        text="Pas de données disponibles pour ces valeurs demandées",
+        xref="paper",
+        yref="paper",
+        showarrow=False,
+        font=dict(
+            size=25
+        )
+         )
+
+    return px.histogram(
+            db[db["LIBREG"].isin(list_reg)],
+            x=annee,
+            barmode="relative",
+            color="LIBREG"
+        ).update_xaxes(
+            range=[0,db[db["LIBREG"].isin(list_reg)][annee].max()+5]
+    )
+
+
+
 @app.callback(
     [Output(component_id="timeline", component_property="figure"),
      Output(component_id="date_inter", component_property="children"),
@@ -147,7 +220,7 @@ def update_figure_timeline(input_value,drop_input):
 
     ordonnee=[]
     for ville in drop_input:
-        ordonnee.append(df[df["LIBZE2020"]==ville].iloc[:,5+input_value[0]:5+input_value[1]].iloc[0])
+        ordonnee.append(df[df["LIBZE2020"]==ville].iloc[:,5+input_value[0]:5+input_value[1]+1].iloc[0])
     #print(ordonnee)
 
     data_f=pd.DataFrame(ordonnee)
@@ -155,27 +228,47 @@ def update_figure_timeline(input_value,drop_input):
 
     data_f.index.name="Date"
     data_f.columns=drop_input
-    fig=px.line(
-        data_f,
-        #x=pd.to_datetime(df.columns[5 + input_value[0]:5 + input_value[1]]),
-        x=data_f.index,
-        y=drop_input,
-        #text=df.iloc[:, 5 + input_value[0]:5 + input_value[1]].iloc[0],
-        markers=True,
 
-        title="Evolution du taux d'emploi",
-        labels={
-            "x":"Années de relevé",
-            "value":"Taux d'emploi (en %)",
-        }
 
-    )
-    fig.update_traces(textposition="top center")
+    if drop_input==[] or data_f.empty:
+        return [
+            px.line(
+                x=None,
+                y=None
+            ).add_annotation(
+        text="Pas de données disponibles pour ces valeurs demandées",
+        xref="paper",
+        yref="paper",
+        showarrow=False,
+        font=dict(
+            size=25
+        )
+         ),
+            f"Impossible d'afficher",
+            f'Pas de villes renseingées'
+        ]
 
-    return [
-        fig,
-    f'Evolution entre ({pd.to_datetime(df.columns[5+input_value[0]])}) à ({pd.to_datetime(df.columns[5+input_value[1]])})',
-        f'Dans la ville de ({drop_input})'
+    else:
+        return [
+            px.line(
+            data_f,
+            # x=pd.to_datetime(df.columns[5 + input_value[0]:5 + input_value[1]]),
+            x=data_f.index,
+            y=drop_input,
+            # text=df.iloc[:, 5 + input_value[0]:5 + input_value[1]].iloc[0],
+            markers=True,
+            title="Evolution du taux d'emploi",
+            labels={
+                "x": "Années de relevé ",
+                "value": "Taux d'emploi (en %) ",
+                "variable":"Ville "
+            }
+
+        ).update_traces(textposition="top center")
+        ,
+    f'Evolution entre ({(df.columns[5+input_value[0]]).split(" ")[0]}) et ({(df.columns[5+input_value[1]]).split(" ")[0]})',
+        #f'Dans la ville de ({" ".join([ville for ville in drop_input])})'
+        f'Evolution du taux de chomage dans certaines villes'
     ]
 app.run_server(debug=True)
 
